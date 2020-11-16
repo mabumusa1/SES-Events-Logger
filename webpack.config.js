6bf1223a4554aeeca5b48bc52979fd78749715ec
Mohammad Abu Musa <m.abumusa@gmail.com>
2020-11-16 14:47:09 +0200

    fix to the test

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const { node } = require("webpack");
+
 module.exports = {
   entry: './src/handlers/index.js',
+  target: 'node',
   output: {
     filename: 'function.js',
     libraryTarget: 'umd'
