59b7cd96c8889cd18ce5475e27d9a0cf5ef51406
Mohammad Abu Musa <m.abumusa@gmail.com>
2020-11-22 22:37:24 +0200

    tests

--- __tests__/unit/handlers/index.test.js
+++ __tests__/unit/handlers/index.test.js
@@ -73,6 +73,7 @@ describe('Test for default-handler', function () {
         delete rec[0].content
         const Item = JSON.parse(JSON.stringify(rec[0]))
         expect(rec[0].published).toBeTruthy()
+        delete rec[0].published
         switch (recordType) {
           case 'bounce':
             if (isNotification) {
